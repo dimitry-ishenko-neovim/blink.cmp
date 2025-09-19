fefd4df41a521a04a0aef5b8c618dad5ea722787
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-09-18 21:17:54 -0400

    Auto-show documentation

--- debian/blink.cmp.lua
+++ debian/blink.cmp.lua
@@ -1,5 +1,6 @@
 require("blink.cmp").setup({
     appearance = {nerd_font_variant = "normal"},
+    completion = {documentation = {auto_show = true}},
     fuzzy = {implementation = "lua"},
     keymap= {
         preset = "enter",
